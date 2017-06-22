8f6368ddf44ddd8e700343b44b11ad6447476097
asweingarten <asweingarten@gmail.com>
2017-06-22 12:40:01 -0700

    Bring in chrome extension code into build folder
    - also remove reference code in Test.elm

---
+++ build/injectElm.js
@@ -0,0 +1,14 @@
+// document.addEventListener('DOMContentLoaded', function() {
+  const eyeGazeOverlay = document.createElement('div');
+  eyeGazeOverlay.id = 'eyegaze-overlay';
+  eyeGazeOverlay.style.position  ='fixed';
+  var w = Math.max(document.documentElement.clientWidth, window.innerWidth || 0);
+  var h = Math.max(document.documentElement.clientHeight, window.innerHeight || 0);
+  eyeGazeOverlay.style.width = w;
+  eyeGazeOverlay.style.height = h;
+  eyeGazeOverlay.style['z-index'] = 9999999;
+
+  document.body.appendChild(eyeGazeOverlay);
+
+  const app = Elm.Main.embed(eyeGazeOverlay);
+// });
