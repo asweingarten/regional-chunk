2e79de93ec7544d0fd37f6e227cb908552bf179b
asweingarten <asweingarten@gmail.com>
2017-07-13 18:01:58 -0700

    Fullscreen progress bar

--- src/view/CommandPalette.elm
+++ src/view/CommandPalette.elm
@@ -20,6 +20,10 @@ view {x, y, sideLength} isActive mActiveCommand=
       case mActiveCommand of
         Nothing -> div [] []
         Just command -> commandProgressDot command x y sideLength
+    progressBar =
+      case mActiveCommand of
+        Nothing -> div [] []
+        Just command -> commandProgressBar command
     myStyle =
       style
         [ ("position", "fixed")
@@ -61,6 +65,7 @@ view {x, y, sideLength} isActive mActiveCommand=
   div []
     [ div [myStyle] []
     , progressDot
+    , progressBar
     , div [westStyle] [text "West"]
     , div [northStyle] [text "North"]
     , div [eastStyle] [text "East"]
@@ -110,6 +115,63 @@ progressDotTop {direction, progress} y dotRadius distanceToEdge =
     Southwest  -> y - dotRadius + ((distanceToEdge // 10 ) * progress)
     East  -> y - dotRadius
     West  -> y - dotRadius
+
+commandProgressBar : DwellCommand -> Html Msg
+commandProgressBar {direction, progress} =
+  let
+    anchor = progressBarAnchor direction
+    width = progressBarWidth direction progress
+    height = progressBarHeight direction progress
+    progressBarStyle =
+      style
+        [ ("background-color", "rgba(25,25,25, 1)")
+        , ("position", "absolute")
+        , anchor
+        , width
+        , height
+        ]
+  in
+  div [progressBarStyle] []
+
+progressBarAnchor : Direction -> (String, String)
+progressBarAnchor direction =
+  case direction of
+    Northwest -> ("bottom", "0")
+    North -> ("bottom", "0")
+    Northeast -> ("bottom", "0")
+    Southeast -> ("top", "0")
+    South -> ("top", "0")
+    Southwest -> ("top", "0")
+    East -> ("left", "0")
+    West -> ("right", "0")
+
+progressBarWidth : Direction -> Int -> (String, String)
+progressBarWidth direction progress =
+  let progressPercent = toString (progress * 10)  ++ "%"
+  in
+  case direction of
+    Northwest -> ("width", "100%")
+    North -> ("width", "100%")
+    Northeast -> ("width", "100%")
+    Southeast -> ("width", "100%")
+    South -> ("width", "100%")
+    Southwest -> ("width", "100%")
+    East -> ("width", progressPercent)
+    West -> ("width", progressPercent)
+
+progressBarHeight : Direction -> Int -> (String, String)
+progressBarHeight direction progress =
+  let progressPercent = toString (progress * 10)  ++ "%"
+  in
+  case direction of
+    Northwest -> ("height", progressPercent)
+    North -> ("height", progressPercent)
+    Northeast -> ("height", progressPercent)
+    Southeast -> ("height", progressPercent)
+    South -> ("height", progressPercent)
+    Southwest -> ("height", progressPercent)
+    East -> ("height", "100%")
+    West -> ("height", "100%")
 
 toPixels : Int -> String
 toPixels int =
