e31bfeb7a390a1c9767a6419865ff1c161b319e7
asweingarten <asweingarten@gmail.com>
2017-07-20 21:12:57 -0700

    East/West progress bars need two anchors
    - was only setting left: 0 and right: 0.
      testing on youtube showed that they need top:0 as well

--- src/view/CommandPalette.elm
+++ src/view/CommandPalette.elm
@@ -141,26 +141,28 @@ commandProgressBar {direction, progress} =
     height = progressBarHeight direction progress
     progressBarStyle =
       style
-        [ ("background-color", "rgba(25,25,25, 0.2)")
-        , ("position", "fixed")
-        , anchor
-        , width
-        , height
-        ]
+        (
+          [ ("background-color", "rgba(25,25,25, 0.2)")
+          , ("position", "fixed")
+          , width
+          , height
+          ]
+          ++ anchor
+        )
   in
   div [progressBarStyle] []
 
-progressBarAnchor : Direction -> (String, String)
+progressBarAnchor : Direction -> List (String, String)
 progressBarAnchor direction =
   case direction of
-    Northwest -> ("bottom", "0")
-    North -> ("bottom", "0")
-    Northeast -> ("bottom", "0")
-    Southeast -> ("top", "0")
-    South -> ("top", "0")
-    Southwest -> ("top", "0")
-    East -> ("left", "0")
-    West -> ("right", "0")
+    Northwest -> [("bottom", "0")]
+    North -> [("bottom", "0")]
+    Northeast -> [("bottom", "0")]
+    Southeast -> [("top", "0")]
+    South -> [("top", "0")]
+    Southwest -> [("top", "0")]
+    East -> [("left", "0"), ("top", "0")]
+    West -> [("right", "0"), ("top", "0")]
 
 progressBarWidth : Direction -> Int -> (String, String)
 progressBarWidth direction progress =
